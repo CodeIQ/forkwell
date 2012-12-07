6b2caed4f2e97bde4f29f3a8c6bafa2bf974a333
Tatsuro Baba <harakirisoul@gmail.com>
2012-12-07 19:29:01 +0900

    [Q1] rails g model publication title url

---
+++ question_1/db/migrate/20121207102748_create_publications.rb
@@ -0,0 +1,10 @@
+class CreatePublications < ActiveRecord::Migration
+  def change
+    create_table :publications do |t|
+      t.string :title
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
