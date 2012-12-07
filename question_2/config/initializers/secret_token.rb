00be137d4f86da039caf06b29163ccef24d6afb7
Tatsuro Baba <harakirisoul@gmail.com>
2012-12-08 01:16:42 +0900

    [Q2] rails new question_2 -T

---
+++ question_2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Question2::Application.config.secret_token = '9f7576377df76ff5138ae3110bc7e8799b2aeea21c21a7cfa8cc150ab7d0d0c1aa098c581f3e423960faaf4244baf42f164d0322005ca43d72061ea1ff277c81'
