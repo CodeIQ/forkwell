3c24ffcf0ca9b859ced7001a097f7eaea649e533
Tatsuro Baba <harakirisoul@gmail.com>
2012-12-09 10:51:10 +0900

    [Q2] add current_user function

--- question_2/app/controllers/application_controller.rb
+++ question_2/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user, :user_signed_in?
+
+  def current_user
+    @current_user ||= User.find(session[:current_user])
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
+
+  def user_signed_in?
+    !!current_user
+  end
 end
