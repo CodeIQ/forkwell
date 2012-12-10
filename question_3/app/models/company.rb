07fdbcb5c4b2d55380664475e713bf8adf55f691
Tatsuro Baba <harakirisoul@gmail.com>
2012-12-10 19:39:28 +0900

    [Q3] add relations between User and Company

--- question_3/app/models/company.rb
+++ question_3/app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ActiveRecord::Base
   attr_accessible :name
+
+  has_many :company_users
+  has_many :users, through: :company_users
 end
--- question_3/app/models/user.rb
+++ question_3/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :username
+
+  has_many :company_users
+  has_many :companies, through: :company_users
 end
