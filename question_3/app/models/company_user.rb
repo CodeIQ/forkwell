b9aefc77e8a84c614e5cd4deb6be840b8a7aeb4e
Tatsuro Baba <harakirisoul@gmail.com>
2012-12-10 19:39:49 +0900

    [Q3] protect is_admin attribute from mass-assignment vulnerability

--- question_3/app/models/company_user.rb
+++ question_3/app/models/company_user.rb
@@ -1,5 +1,7 @@
 class CompanyUser < ActiveRecord::Base
   belongs_to :user
   belongs_to :company
-  attr_accessible :is_admin, :position
+
+  attr_accessible :position
+  attr_accessible :is_admin, :position, as: :admin
 end
