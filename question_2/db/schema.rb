83de2b57a6cc58093ae7ea5870f169c0483df0bb
Akira Matsuda <ronnie@dio.jp>
2012-12-12 16:45:07 +0900

    [Q2] rails g model user username first_name last_name email
    & rake db:migrate

--- question_2/db/schema.rb
+++ question_2/db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121209013317) do
+ActiveRecord::Schema.define(:version => 20121209013512) do
 
   create_table "publications", :force => true do |t|
     t.string   "title"
     t.string   "url"
     t.integer  "category"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
